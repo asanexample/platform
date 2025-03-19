cde210876a68721a3279024e80e150455e2e8091
Josh Deeden <git@deeden.org>
2025-03-19 16:32:36 -0700

    refactor: remove unused _modules directory

--- infra/live/azure/_modules/hosting/variables.tf
+++
@@ -1,9 +0,0 @@
-/**
- * Variables for the hosting composite module
- */
-
-variable "customer" {
-  description = "Customer name to use in resource naming (optional for shared resources)"
-  type        = string
-  default     = null
-} 
